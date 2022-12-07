c888fb6d6d5cb7aaf7102ff2ae16ff99f6a9fd4e
iratze <iratze@DESKTOP-7F4UC40.localdomain>
2022-12-07 12:34:44 +0000

    feature: signup and login added

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
 const usersRouter = require('./user.router.js')
+const authRouter = require('./auth.router.js')
 
 router.use('/users', usersRouter)
+router.use('/auth', authRouter)
 
 module.exports = router
