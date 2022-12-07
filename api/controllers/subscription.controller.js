88f439a180aa52d631f4a8826a3fae0bc0be1bf5
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-07 18:01:59 +0000

    Renamed

--- api/controllers/subscription.controller.js
+++ api/controllers/subscription.controller.js
@@ -1,25 +1,29 @@
 const Subscription = require("../models/subscriptions.model");
 
 function getSubscriptions(req, res) {
-  Subscription.find({ userid: req.params.userid })
+  Subscription
+    .find({ userid: res.locals.user._id })
     .then((sus) => res.json(sus))
     .catch((err) => res.json(err));
 }
 
 function createSubscriptions(req, res) {
-  Subscription.create(req.body)
+  Subscription
+    .create({userid: res.locals.user._id, ...req.body})
     .then((sus) => res.json(sus))
     .catch((err) => res.json(err));
 }
 
 function updateSubscriptions(req, res) {
-  Subscription.updateOne({ _id: req.params.id }, req.body)
+  Subscription
+    .findOneAndUpdate({ _id: req.params.id }, req.body)
     .then((sus) => res.json(sus))
     .catch((err) => res.json(err));
 }
 
 function deleteSubscriptions(req, res) {
-  Subscription.deleteOne({ _id: req.params.id })
+  Subscription
+    .findOneAndDelete({ _id: req.params.id })
     .then((sus) => res.json(sus))
     .catch((err) => res.json(err));
 }
