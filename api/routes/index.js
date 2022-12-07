7af747bb72dee4983819bdda9cb060fd3f1e03b1
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-07 12:52:35 +0000

    ADDED /catalog router

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 
 const usersRouter = require('./user.router.js')
 const authRouter = require('./auth.router.js')
+const catalogRouter = require('./catalog.router.js')
 
 router.use('/users', usersRouter)
 router.use('/auth', authRouter)
+router.use('/catalog', catalogRouter)
 
 module.exports = router
