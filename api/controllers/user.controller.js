cbc774d1b2fb63283793d425c68fa68c20330fe7
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-07 14:00:48 +0000

    MODIFIED removed console.log

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,7 +2,6 @@ const Users = require("../models/user.model");
 //const bcrypt = require('bcrypt') // importamos para encryptar el password
 
 function getUsers(req, res) {
-  console.log('LLEGUÉ AQUÍ')
   Users.find()
     .then(users => res.json(users))
     .catch((err) => res.json(err));
