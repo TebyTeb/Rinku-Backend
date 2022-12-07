d939210decd7b6775fc734992ed8046a3c28e90e
TebyTeb <esteban.c.ojedahdez@gmail.com>
2022-12-07 10:03:09 +0000

    cambio 2

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,5 +3,5 @@ const router = require('express').Router()
 const usersRouter = require('./user.router.js')
 
 router.use('/users', usersRouter)
-console.log('Siete Monos Mas Bonitos')
+console.log('Siete Monos Mas Bonitos que tu')
 module.exports = router
