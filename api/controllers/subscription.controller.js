ca1d2ffe893bbcd58bb4b03597d72fbd0b18f014
iratze <iratze@DESKTOP-7F4UC40.localdomain>
2022-12-11 14:57:13 +0000

    fix: changed sus to subs in subs controller (Ale te mato)

--- api/controllers/subscription.controller.js
+++ api/controllers/subscription.controller.js
@@ -3,28 +3,28 @@ const Subscription = require("../models/subscriptions.model");
 function getSubscriptions(req, res) {
   Subscription
     .find({ userid: res.locals.user._id })
-    .then((sus) => res.json(sus))
+    .then((subs) => res.json(subs))
     .catch((err) => res.json(err));
 }
 
 function createSubscriptions(req, res) {
   Subscription
     .create({userid: res.locals.user._id, ...req.body})
-    .then((sus) => res.json(sus))
+    .then((subs) => res.json(subs))
     .catch((err) => res.json(err));
 }
 
 function updateSubscriptions(req, res) {
   Subscription
     .findOneAndUpdate({ _id: req.params.id }, req.body)
-    .then((sus) => res.json(sus))
+    .then((subs) => res.json(subs))
     .catch((err) => res.json(err));
 }
 
 function deleteSubscriptions(req, res) {
   Subscription
     .findOneAndDelete({ _id: req.params.id })
-    .then((sus) => res.json(sus))
+    .then((subs) => res.json(subs))
     .catch((err) => res.json(err));
 }
 
