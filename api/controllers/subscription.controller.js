8fb2c1681b20ac3486e1cc75a1e1ee9dcbeeceb4
iratze <iratze@DESKTOP-7F4UC40.localdomain>
2022-12-11 20:27:32 +0000

    changed: plan hiring from annually/monthly to year/month

--- api/controllers/subscription.controller.js
+++ api/controllers/subscription.controller.js
@@ -3,9 +3,44 @@ const Subscription = require("../models/subscriptions.model");
 function getSubscriptions(req, res) {
   Subscription
     .find({ userid: res.locals.user._id })
-    .then((subs) => res.json(subs))
-    .catch((err) => res.json(err));
-}
+    .then(subs => {
+
+      let result = []
+
+      subs.map(sub => {
+        let today = new Date()
+        let nextPayment = new Date(sub.hiring_day)
+
+        if(sub.plan.hiring === 'year') {  
+          let totalYears = 0;
+          while(nextPayment < today) {
+            totalYears += 1;
+            nextPayment.setFullYear(nextPayment.getFullYear() + 1);
+          }
+          result.push({...sub._doc, "nextPayment": nextPayment, "totalPaid": totalYears*sub.plan.quantity})
+        }
+
+        if (sub.plan.hiring === 'month') {
+          let totalMonths = 0;
+          while (nextPayment < today) {
+            if(nextPayment.getMonth() === 11) {
+              totalMonths += 1;
+              nextPayment.setFullYear(nextPayment.getFullYear() + 1);
+              nextPayment.setMonth(0);
+            }
+            else if(nextPayment.getMonth() !== 11) {
+              totalMonths += 1;
+              nextPayment.setMonth(nextPayment.getMonth() + 1);
+            }
+          }
+
+          result.push({...sub._doc, "nextPayment": nextPayment, "totalPaid": totalMonths*sub.plan.quantity})
+        }
+      })
+      res.json(result)
+    })
+    .catch((err) => res.json(err));  
+    }
 
 function createSubscriptions(req, res) {
   Subscription
